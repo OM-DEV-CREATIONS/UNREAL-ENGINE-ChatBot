86d0e9bdbab32942dea6bae43211c86c7e88b3fc
OM-DEV-CREATIONS <prakashraj7785@gmail.com>
2026-03-29 12:25:55 +0530

    Source File

---
+++ CPPGameMode_header.h
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UnrealCPPGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealCPPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealCPPGameMode();
+};
